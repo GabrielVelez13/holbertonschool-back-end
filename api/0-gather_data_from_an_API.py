f4f9e46d9fe4c3e0357eb8a1aa890d871e6fba76
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-04-16 03:05:29 -0400

    adding task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" 
+This script gets data from a todo list through
+a REST API protocol and presents useful info.  
+"""
+import requests
+import sys
+
+def get_todo_data(emp_ID):
+    """ Gets data from url and makes that data into JSON. """
+    url = "https://jsonplaceholder.typicode.com"
+    user_url = url + f"/users/{emp_ID}"
+    todo_url = url + f"/users/{emp_ID}/todos"
+    response1 = requests.get(user_url)
+    user = response1.json()
+    response2 = requests.get(todo_url)
+    todos = response2.json()
+
+    """ Parses data. """
+    complete = 0
+    total_todos = len(todos)
+    completed_titles = []
+    for todo in todos:
+        if todo['completed']:
+            complete += 1
+            completed_titles.append(todo["title"])
+
+    """ Prints data. """
+    print(f"Employee {user["name"]} is done with tasks({complete}/{total_todos}):")
+    for titles in completed_titles:
+        print(f"\t {titles}")
+
+if __name__ == "__main__":
+    get_todo_data(sys.argv[1])
---
+++ api/README.md
@@ -0,0 +1,18 @@
+# API with Python
+
+## General
+- What Bash scripting should not be used for?
+- What is an API?
+- What is a REST API?
+- What are microservices?
+- What is the CSV format?
+- What is the JSON format?
+
+## Pythonic Naming Conventions
+- Package and module name style
+- Class name style
+- Variable name style
+- Function name style
+- Constant name style
+
+## Significance of CapWords or CamelCase in Python
