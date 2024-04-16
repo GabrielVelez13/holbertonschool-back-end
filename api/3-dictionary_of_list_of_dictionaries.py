431804ff0a4c2f772d0ccdc6c57a8146b6f982b9
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-04-16 18:16:27 -0400

    adding task 3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This script gets data from a todo list through
+a REST API protocol and presents useful info, 
+into a JSON file with all employees.
+"""
+import json
+import requests
+
+
+def get_todo_data():
+    """ Gets data from url and makes that data into JSON. """
+    url = "https://jsonplaceholder.typicode.com"
+    user_url = f"/users/"
+    todo_url = url + f"/users/todos"
+
+    response1 = requests.get(url + user_url)
+    users = response1.json()
+
+    """ Pasing data"""
+    data = {}
+    for user in users:
+        response2 = requests.get(
+            url + user_url + str(user['id']) + "/todos"
+        )
+        todos = response2.json()
+        list_data = []
+        for todo in todos:
+            task_data = {
+                "username": user['username'],
+                "task": todo['title'],
+                "completed": todo['completed'],
+                }
+            list_data.append(task_data)
+        data[user['id']] = list_data
+
+    """ Export data to JSON """
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(data, f)
+
+
+if __name__ == "__main__":
+    get_todo_data()
