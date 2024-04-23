b9423cef9df1013a4f5e1a48b690d8d8f21cff5a
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-04-23 15:28:13 -0400

    adding task 1 - 2

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" A strictly typed functions """
+""" A staticly typed add function """
 
 def add(a: float, b: float) -> float:
     """ Add two float and return them """
---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+""" Staticly typed floor division """
+
+def floor(num: float) -> int:
+    return int(num // 1)
